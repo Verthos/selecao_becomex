31289cf8a30f9ef597e9631d4773b7382e7cdfad
unknown <wpkszan@gmail.com>
2021-12-08 11:00:47 -0300

    fixing country map

--- src/components/Countries/index.jsx
+++ src/components/Countries/index.jsx
@@ -10,7 +10,7 @@ export function Countries({countries}) {
             {            
             
             countries.map(country => {return(
-                <li className={styles.country}>
+                <li key={country.name} className={styles.country}>
                     <img src={country.flags.svg} alt="country_flag" />
                     
                     <ul>
