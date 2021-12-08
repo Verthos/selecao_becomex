0c5ad059cded56a205a4ca7f44f73801eaa828c5
unknown <wpkszan@gmail.com>
2021-12-08 18:01:02 -0300

    fixing deploy bugs

--- src/components/Countries/index.jsx
+++ src/components/Countries/index.jsx
@@ -17,14 +17,7 @@ export function Countries({countries}) {
                             <li>Nome: {country.name.common}</li>
                             <li>Sigla: {country.cca2}</li>
                             <li>Nome: {country.name.common}</li>
-                            <li> Moeda: 
-                                {country.currencies !== undefined ? 
-                                <>
-                                    <span className={styles.coinSymbol}>{country.currencies[Object.keys(country.currencies)[0]].symbol}</span>
-                                    <span className={styles.coinName}>{country.currencies[Object.keys(country.currencies)[0]].name}</span>
-                                </>
-                                : "Sem moeda informada"}
-                            </li>
+                            
                         </ul>
                     </li>
                 </a>
