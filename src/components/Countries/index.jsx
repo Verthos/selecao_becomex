4c72f819cb9cedde8335baf7e4e125e93322b337
unknown <wpkszan@gmail.com>
2021-12-08 18:02:08 -0300

    fixing deploy bugs

--- src/components/Countries/index.jsx
+++ src/components/Countries/index.jsx
@@ -9,7 +9,7 @@ export function Countries({countries}) {
 
             {            
             countries.map(country => {return(
-                <a key={country.name} className={styles.card} href={`/${country.name.common}`}>
+                
                     <li  className={styles.country}>
                         <img src={country.flags.svg} alt="country_flag" />
                         
@@ -17,10 +17,16 @@ export function Countries({countries}) {
                             <li>Nome: {country.name.common}</li>
                             <li>Sigla: {country.cca2}</li>
                             <li>Nome: {country.name.common}</li>
-                            
+                            <li> Moeda: 
+                                {country.currencies !== undefined ? 
+                                <>
+                                    <span className={styles.coinSymbol}>{country.currencies[Object.keys(country.currencies)[0]].symbol}</span>
+                                    <span className={styles.coinName}>{country.currencies[Object.keys(country.currencies)[0]].name}</span>
+                                </>
+                                : "Sem moeda informada"}
+                            </li>
                         </ul>
                     </li>
-                </a>
                     
             )})}
             
